e3d6985cff734754ea8a810daf113a55d514df83
Hebi Li <lihebi.com@gmail.com>
2017-12-20 14:29:13 -0600

    setup dual monitor

--- monitor.lisp
+++ monitor.lisp
@@ -1,13 +1,21 @@
-(defvar *monitor* "VGA-1")
-(setq *monitor* "DP-2")
+;; direction: left, right, normal
+(defun rotate (monitor direction)
+  (run-shell-command
+   (concatenate 'string "xrandr --output " monitor " --rotate " direction)))
 
-(defun rotate(direction) ()
-       (run-shell-command
-        (concatenate 'string "xrandr --output " *monitor* " --rotate " direction)))
+(defun left-of (l r)
+  (run-shell-command
+   (concatenate 'string "xrandr --output " l " --left-of " r)))
 
-(defcommand rotate-left() ()
-            (rotate "left"))
-(defcommand rotate-right() ()
-            (rotate "right"))
-(defcommand rotate-normal() ()
-            (rotate "normal"))
+(defun 144-hz (monitor)
+  (run-shell-command
+   (concatenate 'string "xrandr --output " monitor )))
+
+;; xrandr --output DP-0 --rotate left --left-of DP-2
+;; xrandr --output DP-2 --mode 2560x1440 --rate 144
+
+(defvar *left-monitor* "DP-0")
+(defvar *right-monitor* "DP-2")
+
+(rotate *left-monitor* "left")
+(left-of *left-monitor* *right-monitor*)
